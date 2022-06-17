7be0a512783876b96a1dbdeea8f64c3b64c51582
WuFenghong <1611330@mail.nankai.edu.cn>
2022-06-17 21:25:55 +0800

    20220617 code daily LC_1089_duplicateZeros

--- JZOF/JZOF_2_25_addTwoNumbers.cpp
+++ JZOF/JZOF_2_25_addTwoNumbers.cpp
@@ -1,3 +1,110 @@
 //
 // Created by WuFenghong on 2022/6/16.
 //
+//
+// Created by WuFenghong on 2022/6/16.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1->val == 0 || l2->val == 0) return l1->val == 0 ? l2 : l1;
+        stack<ListNode*> s1, s2;
+        auto size1 = 0, size2 = 0;
+        auto temp = l1;
+        while (temp) {
+            s1.push(temp);
+            temp = temp->next;
+            ++size1;
+        }
+        temp = l2;
+        while (temp) {
+            s2.push(temp);
+            temp = temp->next;
+            ++size2;
+        }
+        if (size1 > size2) {
+            auto carry = 0;
+            while (!s2.empty()) {
+                auto p1 = s1.top(), p2 = s2.top();
+                auto sum = p1->val + p2->val + carry;
+                if (sum > 9) {
+                    p1->val = sum - 10;
+                    carry = 1;
+                }
+                else {
+                    p1->val = sum;
+                    carry = 0;
+                }
+                s1.pop();
+                s2.pop();
+            }
+            while (carry != 0 && !s1.empty()) {
+                auto p = s1.top();
+                auto sum = p->val + carry;
+                if (sum > 9) {
+                    p->val = sum - 10;
+                    carry = 1;
+                }
+                else {
+                    p->val = sum;
+                    carry = 0;
+                }
+                s1.pop();
+            }
+            if (carry != 0) {
+                ListNode* ans = new ListNode(1);
+                ans->next = l1;
+                return ans;
+            }
+            return l1;
+        }
+        else {
+            auto carry = 0;
+            while (!s1.empty()) {
+                auto p1 = s2.top(), p2 = s1.top();
+                auto sum = p1->val + p2->val + carry;
+                if (sum > 9) {
+                    p1->val = sum - 10;
+                    carry = 1;
+                }
+                else {
+                    p1->val = sum;
+                    carry = 0;
+                }
+                s1.pop();
+                s2.pop();
+            }
+            while (carry != 0 && !s2.empty()) {
+                auto p = s2.top();
+                auto sum = p->val + carry;
+                if (sum > 9) {
+                    p->val = sum - 10;
+                    carry = 1;
+                }
+                else {
+                    p->val = sum;
+                    carry = 0;
+                }
+                s2.pop();
+            }
+            if (carry != 0) {
+                ListNode* ans = new ListNode(1);
+                ans->next = l2;
+                return ans;
+            }
+            return l2;
+        }
+    }
+};
---
+++ code daily/LC_1089_duplicateZeros.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<math.h>
+#include<unordered_map>
+#include<stack>
+#include<map>
+#include<functional>
+#include<queue>
+using namespace std;
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//Definition for singly - linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* buildListNode(vector<int> nums) {
+    ListNode* a = new ListNode(nums[0]);
+    ListNode* temp = a;
+    for (int i = 1; i < nums.size(); i++) {
+        temp->next = new ListNode(nums[i]);
+        temp = temp->next;
+    }
+    return a;
+}
+
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int m = arr.size(), low = 0, high = m - 1, count = 0;
+        for (low = 0; low < high; ++low) {
+            if (arr[low] == 0) {
+                ++count;
+                --high;
+            }
+        }
+        if (high == m - 1) return;
+        if (arr[low] == 0) ++count;
+        auto seat = m - high - 1;
+        low = high, high = m - 1;
+        while (low >= 0 && high >= 0) {
+            arr[high] = arr[low];
+            if (arr[low] == 0) {
+                if (count <= seat) {
+                    --high;
+                    if (high >= 0)
+                        arr[high] = arr[low];
+                }
+                else --count;
+            }
+            --high;
+            --low;
+        }
+        return;
+    }
+};
+
+int main()
+{
+    vector v{9,0,9,0,6,0,0,0,1,1,6,5,4,4,8,3,0,0,0,1,5,3,0,0,7,2,1,0,2,0,9,1,0,2,0,0,0,0,0,0,0,6,0,0,7,9,0,8,7,0,9,7,1,0,2,0,0,0,0,9,0,0,0,0};
+    Solution s;
+    s.duplicateZeros(v);
+}
